7c2d475932cea74d06cc70f9a0900d09c382ba6e
Vinícius Sato <vinicius.sato@reserve.com.br>
2024-10-08 02:32:18 -0300

    put codigo produto

--- UrbanFarmingAPI/Controllers/ProdutosController.cs
+++ UrbanFarmingAPI/Controllers/ProdutosController.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using UrbanFarming.Domain.Classes;
 using UrbanFarming.Domain.Exceptions;
 using UrbanFarming.Domain.Interfaces.Services;
--- UrbanFarmingAPI/obj/Debug/net8.0/UrbanFarmingAPI.AssemblyInfo.cs
+++ UrbanFarmingAPI/obj/Debug/net8.0/UrbanFarmingAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrbanFarmingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842cb379a6e16d219a9f79209c31eb056d08ad39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc2d878d0b5bb868c8b51111f5110743de6e464f")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrbanFarmingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrbanFarmingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
