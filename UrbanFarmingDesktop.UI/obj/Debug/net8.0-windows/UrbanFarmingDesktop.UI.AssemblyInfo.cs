2619622585f477591ae9c334d4553c6a5a6bda76
thiagonuke <mthiago299@gmail.com>
2024-10-10 01:30:42 -0300

    .

--- UrbanFarmingDesktop.UI/obj/Debug/net8.0-windows/UrbanFarmingDesktop.UI.AssemblyInfo.cs
+++ UrbanFarmingDesktop.UI/obj/Debug/net8.0-windows/UrbanFarmingDesktop.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrbanFarmingDesktop.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6997b64fbf16ba60a874089636efc1fc21b2d413")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ebd571d9fb2befa901e448867fe1c0687d90672")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrbanFarmingDesktop.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrbanFarmingDesktop.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
